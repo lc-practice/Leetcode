f371a5b16fd7750c7c4d584705d71c0cd2916222
cookietough <103462001+cookietough@users.noreply.github.com>
2022-04-13 18:02:02 -0400

    Update kobe_2103.py
    added a new version using defaultdict to save assigning empty list to a dictionary in a for loop. time/space complexity didn't change much

--- 2013/kobe_2103.py
+++ 2013/kobe_2103.py
@@ -1,3 +1,4 @@
+# 35 ms	13.8 MB
 class Solution:
     def countPoints(self, rings: str) -> int:
         rods = {}
@@ -5,6 +6,21 @@ class Solution:
         num_rod = len(rings)
         for i in range(0,num_rod, 2):
             rods[rings[i+1]] = []
+        for i in range(0,num_rod, 2):
+            rods[rings[i+1]].append(rings[i])
+        for i in rods.keys():
+            if len(set(rods[i]))==3:
+                all_color.append(i)
+        return len(all_color)
+
+# 37 ms	13.8 MB
+class Solution:
+    def countPoints(self, rings: str) -> int:
+        def def_value():
+            return []
+        rods = defaultdict(def_value)
+        all_color = []
+        num_rod = len(rings)
         for i in range(0,num_rod, 2):
             rods[rings[i+1]].append(rings[i])
         for i in rods.keys():
