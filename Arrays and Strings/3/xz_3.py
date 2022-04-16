fd82a035cd379c00b3bb832b96c33e8ae58d8f5e
lc-practice <103459687+lc-practice@users.noreply.github.com>
2022-04-15 21:04:29 -0400

    Update xz_3.py

--- Arrays and Strings/3/xz_3.py
+++ Arrays and Strings/3/xz_3.py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        n = len(s)
+        ans = 0
+        mp = set()
+        left = 0
+        for right in range(n):
+            while s[right] in mp:
+                mp.remove(s[left])
+                left += 1
+            mp.add(s[right])
+            
+            ans = max(ans, right - left + 1)
+        return ans
+    
