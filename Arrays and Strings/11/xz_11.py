77be0d1fb053eeebf6dfe19643307a36e4280006
lc-practice <103459687+lc-practice@users.noreply.github.com>
2022-04-21 08:23:14 -0400

    Update xz_11.py

--- Arrays and Strings/11/xz_11.py
+++ Arrays and Strings/11/xz_11.py
@@ -1,20 +1,12 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        res = []
-        for i in range(len(nums)):
-            if i == 0 or nums[i] != nums[i - 1]:
-                for output in self.twoSum(nums, i + 1, 0 - nums[i]):
-                    res.append([nums[i]] + output)
-        return res
-    
-    def twoSum(self, nums, start, target):
-        result = []
-        s = set()
-        for i in range(start, len(nums)):
-            if len(result) == 0 or result[-1][1] != nums[i]:
-                complement = target - nums[i]
-                if complement in s:
-                    result.append([complement, nums[i]])
-                s.add(nums[i])
-        return result
+    def maxArea(self, height: List[int]) -> int:
+        maxArea = 0
+        left = 0
+        right = len(height) - 1
+        while left < right:
+            maxArea = max(maxArea, min(height[left], height[right]) * (right - left))
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return maxArea
